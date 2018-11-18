1ba067b56d7957705b5de7ed63053efed7791ff4
Jared Cuffe <jcuffe@gmail.com>
2018-11-17 16:11:02 -0800

    Major redesign to a dumb translator

--- endpoints.js
+++ endpoints.js
@@ -4,6 +4,7 @@ const endpoints = {
   corpAssets: (corporationId) => apiUrl + `corporations/${corporationId}/assets`,
   corpBlueprints: (corporationId) => apiUrl + `corporations/${corporationId}/blueprints`,
   corpTransactions: (corporationId, walletDivision = 1) => apiUrl + `corporations/${corporationId}/wallets/${walletDivision}/transactions`,
+  regionOrders: (regionID, typeID) => apiUrl + `markets/${regionID}/orders/?type_id=${typeID}`,
   names: apiUrl + "universe/names",
   systemIds: apiUrl + "universe/systems",
   structureIds: apiUrl + "universe/structures",
--- server.js
+++ server.js
@@ -2,39 +2,36 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const endpoints = require('./endpoints');
-const { 
-  lookupTypeMaterials, 
-  lookupTypes,
-} = require('./fetchers');
 
 const app = express();
 app.use(cors());
 
-const asyncMiddleware = (fn) =>
-  (req, res, next) => 
-    Promise
-      .resolve(fn(req, res, next))
-      .catch(next);
-
-app.get('/transactions/:corpID', asyncMiddleware(async (req, res) => {
-  const { authorization } = req.headers;
-  const { corpID } = req.params;
-  const options = { headers: { authorization } };
-  const { data: corpAssets } = await axios.get(endpoints.corpAssets(corpID), options);
-  const corpTransactions = fetchCorpTransactions(corpID, options);
-  res.json({ corpAssets, corpTransactions });
-}));
-
-app.get('/materials/:corpID/:typeID', asyncMiddleware(async (req, res) => {
-  const { corpID, typeID } = req.params;
-  const { authorization } = req.headers;
-  const options = { headers: { authorization } };
-  res.json(await lookupTypeMaterials(corpID, typeID, options));
-}));
-
-app.get('/types', asyncMiddleware(async (req, res) => {
-  res.json(await lookupTypes());
-}));
+app.get('/', (req, res) => {
+  res.json({ "hello": "hello" });
+})
+app.get('/market', (req, res) => {
+  const types = req.query.types.split(',');
+  const requests = types.map(type => {
+    return axios.get(endpoints.regionOrders(10000060, type))
+  });
+  const response = {};
+  Promise.all(requests)
+    .then(responses => responses.map(({ data }) => data))
+    .then(types => types.forEach(orders => {
+      orders.forEach(order => console.log(order));
+      const topBuy = orders
+        .filter(order => order.is_buy_order)
+        .reduce((max, curr) => curr.price > max.price ? curr : max)
+        .price;
+      const bottomSell = orders
+        .filter(order => !order.is_buy_order)
+        .reduce((min, curr) => curr.price < min.price ? curr : min)
+        .price;
+      const typeID = orders[0].type_id;
+      response[typeID] = { topBuy, bottomSell };
+    }))
+    .then(() => res.json(response));
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("listening..."));
+app.listen(port, () => "Server started");
